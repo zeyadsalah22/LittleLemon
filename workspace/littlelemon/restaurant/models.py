c73441d3b0513a6a06868e47909055e154383cba
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2024-09-03 17:10:56 +0300

    testing

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -7,7 +7,13 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} : {self.booking_date}"
--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu/', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view(), name='menu'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth',obtain_auth_token)
 ]
